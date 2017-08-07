6062249f9ddf0f7231d38d837b1304712f98f060
amansawhney <amansawhney@sawhneygames.com>
2017-08-07 12:20:51 -0400

    added error handling

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.get('/', (req, res) => {
   res.send("This is an API not an app, don't try and get me!");
 });
 
-const PORT =  process.env.PORT || 3000;
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
