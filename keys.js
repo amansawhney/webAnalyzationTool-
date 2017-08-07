56c559f14a10962f7552f8c2511193589c246690
amansawhney <amansawhney@sawhneygames.com>
2017-08-07 09:45:29 -0400

    Finished PSI

---
+++ keys.js
@@ -0,0 +1,3 @@
+module.export = {
+    googlePSIKEY: "AIzaSyA-pZigox9QY2e3BN_fxpgvsH0teRToLu4"
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
 const psi = require('psi');
+const keys = require('../keys');
 
 //middleWare
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/psi', (req, res) => {
-  psi('theverge.com').then(data => {
-    res.json(data.ruleGroups)
+app.post('/psi', (req, res) => {
+  var data = {
+    moblieSpeed: '',
+    moblieUsability: '',
+    Speed: '',
+  };
+  psi(req.body.url, {
+    strategy: 'mobile',
+  }).then(moblie => {
+    console.log(moblie);
+    data.moblieSpeed = moblie.ruleGroups.SPEED.score;
+    data.moblieUsability = moblie.ruleGroups.USABILITY.score;
+    psi(req.body.url, {
+      strategy: 'desktop',
+    }).then(desktop => {
+      console.log(req.body.url);
+      console.log(desktop);
+      data.Speed = desktop.ruleGroups.SPEED.score;
+      res.send(data);
+    });
   });
 });
 
