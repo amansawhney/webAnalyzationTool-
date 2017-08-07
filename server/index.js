5a24eab7942fad69dda9e3fd8ec92717de181058
amansawhney <amansawhney@sawhneygames.com>
2017-08-07 09:16:27 -0400

    got PSI to respond with json

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const psi = require('psi');
 
+//middleWare
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/psi', (req, res) => {
+  psi('theverge.com').then(data => {
+    res.json(data.ruleGroups)
+  });
+});
 
 app.listen(3000);
