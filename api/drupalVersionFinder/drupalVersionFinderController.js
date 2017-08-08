301614ed0abfbca29a858366f892b9db4e55d7a7
amansawhney <amansawhney@sawhneygames.com>
2017-08-07 22:25:28 -0400

    cleaned up droupal api

--- api/drupalVersionFinder/drupalVersionFinderController.js
+++ api/drupalVersionFinder/drupalVersionFinderController.js
@@ -1,18 +1,27 @@
 const request = require('request');
+const check = require('httpcheck');
+
 exports.getDropalVersion = (req, res, next) => {
-  request(
-    {
-      url: 'https://' + req.body.url + '/CHANGELOG.txt',
-      rejectUnauthorized: false,
-    },
-    function(error, response, body) {
-      if (error) {
-        next(error);
-      } else {
-        body.includes('drupal')
-          ? res.send({ version: body.slice(8, 12) })
-          : res.send({ version: false });
-      }
-    },
-  );
+  check({ url: 'http://' + req.body.url + '/CHANGELOG.txt' }, function(err) {
+    if (err) {
+      console.log('HTTP check for example.com failed!');
+      res.send({ version: 'false' });
+    } else {
+      request(
+        {
+          url: 'https://' + req.body.url + '/CHANGELOG.txt',
+          rejectUnauthorized: false,
+        },
+        function(error, response, body) {
+          if (error) {
+            next(error);
+          } else {
+            body.includes('drupal')
+              ? res.send({ version: body.slice(8, 12) })
+              : res.send({ version: false });
+          }
+        },
+      );
+    }
+  });
 };
--- api/wpVersionFinder/wpVersionFinderController.js
+++ api/wpVersionFinder/wpVersionFinderController.js
@@ -6,21 +6,28 @@ exports.getVersion = (req, res, next) => {
   check({ url: 'http://' + req.body.url + '/feed' }, function(err) {
     if (err) {
       console.log('HTTP check for example.com failed!');
-      res.send('False');
-    }
-    osmosis
-      .get('http://' + req.body.url + '/feed')
-      .find('body')
-      .set('location')
-      .data(function(data) {
-        res.json({
-          version: data.location
-            .substring(data.location.lastIndexOf('https://wordpress.org/?') + 1)
-            .slice(24, 29)
-            .replace('\n', '')
-            .replace('\t', ''),
+      res.send({ version: 'false' });
+    } else {
+      osmosis
+        .get('http://' + req.body.url + '/feed')
+        .find('body')
+        .set('location')
+        .data(function(data) {
+          if (data.location.lastIndexOf('https://wordpress.org/?') === -1) {
+            res.send({ version: 'false' });
+          } else {
+            res.json({
+              version: data.location
+                .substring(
+                  data.location.lastIndexOf('https://wordpress.org/?') + 1,
+                )
+                .slice(24, 29)
+                .replace('\n', '')
+                .replace('\t', ''),
+            });
+          }
         });
-      });
+    }
     console.log('HTTP check for example.com has passed');
   });
 };
