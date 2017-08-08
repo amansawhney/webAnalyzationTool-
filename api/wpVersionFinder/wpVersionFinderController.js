67f6f22a83ff7264916712b3deb9ea4fece50123
amansawhney <amansawhney@sawhneygames.com>
2017-08-07 22:36:47 -0400

    cleaned up wpcheck code

--- api/wpVersionFinder/wpVersionFinderController.js
+++ api/wpVersionFinder/wpVersionFinderController.js
@@ -1,33 +1,22 @@
-const osmosis = require('osmosis');
+const request = require('request');
 const check = require('httpcheck');
 
 exports.getVersion = (req, res, next) => {
   var error = true;
-  check({ url: 'http://' + req.body.url + '/feed' }, function(err) {
-    if (err) {
-      console.log('HTTP check for example.com failed!');
-      res.send({ version: 'false' });
-    } else {
-      osmosis
-        .get('http://' + req.body.url + '/feed')
-        .find('body')
-        .set('location')
-        .data(function(data) {
-          if (data.location.lastIndexOf('https://wordpress.org/?') === -1) {
-            res.send({ version: 'false' });
-          } else {
-            res.json({
-              version: data.location
-                .substring(
-                  data.location.lastIndexOf('https://wordpress.org/?') + 1,
-                )
-                .slice(24, 29)
-                .replace('\n', '')
-                .replace('\t', ''),
-            });
-          }
+  request(
+    {
+      url: 'https://' + req.body.url + '/feed',
+      rejectUnauthorized: false,
+    },
+    function(error, response, body) {
+      console.log(body);
+      if (body.lastIndexOf('https://wordpress.org/?') < 0) {
+        res.send({ version: 'false' });
+      } else {
+        res.json({
+          version: body.substring(body.lastIndexOf("https://wordpress.org/?")+25, body.lastIndexOf("https://wordpress.org/?")+30)
         });
-    }
-    console.log('HTTP check for example.com has passed');
-  });
+      }
+    },
+  );
 };
