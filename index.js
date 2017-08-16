8a29c1c9bc963038a551ad618e927e2ddd3c6d82
amansawhney <amansawhney@sawhneygames.com>
2017-08-16 13:04:50 -0400

    added build script to appliction for heokuapp

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ require('./middleware')(app);
 //routes
 require('./routes')(app);
 
-
-app.get('/', (req, res) => {
-  res.send("This is an API not an app, don't try and get me!");
-});
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (res, req) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 
