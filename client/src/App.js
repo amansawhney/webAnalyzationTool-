ac201939043aa9436bd2a499d8edcf2b85d80062
amansawhney <amansawhney@sawhneygames.com>
2017-08-16 12:07:45 -0400

    made a quick client for fetching the PSI route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      data: {
+        psi: {},
+      },
+    };
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.postAPI = this.postAPI.bind(this);
+  }
+  postAPI(e) {
+    e.preventDefault();
+    var self = this;
+    axios
+      .post('/api/psi', {
+        url: self.state.url,
+      })
+      .then(function(response) {
+        console.log(response.data);
+        self.setState({ data: { psi: response.data } });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  handleUrlChange(event) {
+    this.setState({ url: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <input
+          type="text"
+          value={this.state.url}
+          onChange={this.handleUrlChange}
+        />
+        <button onClick={this.postAPI}>postAPI</button>
+        <p>
+          {JSON.stringify(this.state.data.psi, null, 2)}
         </p>
       </div>
     );
