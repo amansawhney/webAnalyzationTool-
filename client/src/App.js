6248f816ea09ecd52363a5c066830ddab4baa510
amansawhney <amansawhney@sawhneygames.com>
2017-08-16 16:20:20 -0400

    Added routes to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+const mergeJSON = require('merge-json');
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       url: '',
-      data: {
-        psi: {},
-        ada: {},
-      },
+      data: {},
     };
     this.handleUrlChange = this.handleUrlChange.bind(this);
     this.postAPI = this.postAPI.bind(this);
@@ -17,28 +15,32 @@ class App extends Component {
   postAPI(e) {
     e.preventDefault();
     var self = this;
-    axios
-      .post('/api/psi', {
-        url: self.state.url,
-      })
-      .then(function(response) {
-        console.log(response.data);
-        self.setState({ data: { psi: response.data } });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    axios
-      .post('/api/ada', {
-        url: self.state.url,
-      })
-      .then(function(response) {
-        console.log(response.data);
-        self.setState({ data: { ada: response.data } });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    var routes = [
+      'ada',
+      'psi',
+      'links',
+      'tags',
+      'ssl',
+      'wpVersion',
+      'drupalVersion',
+    ];
+    routes.map(route => {
+      axios
+        .post('/api/' + route, {
+          url: self.state.url,
+        })
+        .then(function(response) {
+          console.log(response.data);
+          self.setState({
+            data: mergeJSON.merge(self.state.data, {
+              data: { [route]: response.data },
+            }),
+          });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
   }
   handleUrlChange(event) {
     this.setState({ url: event.target.value });
@@ -53,9 +55,11 @@ class App extends Component {
           onChange={this.handleUrlChange}
         />
         <button onClick={this.postAPI}>postAPI</button>
-        <p>
-          {JSON.stringify(this.state.data, null, 2)}
-        </p>
+        <pre>
+          <code>
+            {JSON.stringify(this.state.data, null, 2)}
+          </code>
+        </pre>
       </div>
     );
   }
