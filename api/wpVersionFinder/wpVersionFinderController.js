bcc5832e9002e190da99d67d2760dc09555a5d07
amansawhney <amansawhney@sawhneygames.com>
2017-08-18 18:32:42 -0400

    Added latest version detechtion to wp

--- api/wpVersionFinder/wpVersionFinderController.js
+++ api/wpVersionFinder/wpVersionFinderController.js
@@ -1,28 +1,57 @@
 const request = require('request');
 const check = require('httpcheck');
+const cheerio = require('cheerio');
 
 exports.getVersion = (req, res, next) => {
-  var error = true;
   request(
     {
       url: 'https://' + req.body.url + '/feed',
       rejectUnauthorized: false,
     },
     function(error, response, body) {
-      console.log(body);
-      if(!body) {
-          res.send({ version: 'false' });
+      if (error) {
+        next(error);
       }
-      else if (body.lastIndexOf('https://wordpress.org/?') < 0) {
+      if (!body) {
+        res.send({ version: 'false' });
+      } else if (body.lastIndexOf('https://wordpress.org/?') < 0) {
         res.send({ version: 'false' });
       } else {
-        res.json({
-          version: body
-            .substring(
-              body.lastIndexOf('https://wordpress.org/?') + 25,
-              body.lastIndexOf('https://wordpress.org/?') + 30,
-            ).replace('</', ''),
-        });
+        var currentVersion = body
+          .substring(
+            body.lastIndexOf('https://wordpress.org/?') + 25,
+            body.lastIndexOf('https://wordpress.org/?') + 30,
+          )
+          .replace('</', '');
+        var latestVersion = 'hi';
+        request(
+          {
+            url: 'https://wordpress.org/news/category/releases/',
+            rejectUnauthorized: false,
+          },
+          function(error, response, body) {
+            if (!error) {
+              var $ = cheerio.load(body);
+              $(
+                '#pagebody > div > div > table > tbody > tr:nth-child(1) > td > a',
+              ).filter(function() {
+                var data = $(this);
+                latestVersion = data['0'].attribs.href
+                  .substring(
+                    data['0'].attribs.href.lastIndexOf('wordpress-') + 10,
+                    data['0'].attribs.href.lastIndexOf('wordpress-') + 15,
+                  )
+                  .replace(/-/g, '.')
+                  .replace('/[A-Za-z]/g', '');
+                res.json({
+                  version: currentVersion,
+                  latest: latestVersion,
+                  isOnLatestVersion: currentVersion == latestVersion,
+                });
+              });
+            }
+          },
+        );
       }
     },
   );
