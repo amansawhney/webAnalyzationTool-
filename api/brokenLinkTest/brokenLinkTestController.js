383442a5041b8041d20216eec16e18873da48a46
amansawhney <amansawhney@sawhneygames.com>
2017-08-23 16:25:54 -0400

    limited URLs tested

--- api/brokenLinkTest/brokenLinkTestController.js
+++ api/brokenLinkTest/brokenLinkTestController.js
@@ -17,20 +17,27 @@ exports.getBrokenLinks = (req, res, next) => {
       html: function(tree, robots, response, pageUrl, customData) {},
       junk: function(result, customData) {},
       link: function(result, customData) {
-        console.log(result);
+        console.log(data.sucessUrls.length);
         if (!result.broken) {
           data.sucessUrls.push(result.url.resolved);
         } else {
           data.faliureUrls.push(result.url.resolved);
         }
+        if (data.sucessUrls.length > 100) {
+          data.numberOfFailed = data.faliureUrls.length;
+          data.numberOfSucess = data.sucessUrls.length;
+          res.send(data);
+        }
       },
       page: function(error, pageUrl, customData) {},
       site: function(error, siteUrl, customData) {},
       end: function() {
-        data.numberOfFailed = data.faliureUrls.length;
-        data.numberOfSucess = data.sucessUrls.length;
-        res.send(data);
-        console.log('done');
+        if (data.sucessUrls.length < 100) {
+          data.numberOfFailed = data.faliureUrls.length;
+          data.numberOfSucess = data.sucessUrls.length;
+          res.send(data);
+          console.log('done');
+        }
       },
     },
   );
