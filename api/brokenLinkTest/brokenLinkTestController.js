5009752c251e1d6925679fffbf442cbd2bf5449b
amansawhney <amansawhney@sawhneygames.com>
2017-08-23 15:53:58 -0400

    finished broken links testing:

--- api/brokenLinkTest/brokenLinkTestController.js
+++ api/brokenLinkTest/brokenLinkTestController.js
@@ -1,9 +1,41 @@
 const findlinks = require('findlinks');
+const blc = require('broken-link-checker');
 
-exports.getBrokenLinks = (req, res) => {
-  findlinks({ src: 'http://' + req.body.url })
-    .then(result => res.send(result))
-    .catch(err => next(err));
+exports.getBrokenLinks = (req, res, next) => {
+  var data = {
+    sucessUrls: [],
+    faliureUrls: [],
+    numberOfSucess: 0,
+    numberOfFailed: 0,
+  };
+  var siteChecker = new blc.SiteChecker(
+    {
+      'user-agent': 'node-spider',
+      rejectUnauthorized: false,
+    },
+    {
+      robots: function(robots, customData) {},
+      html: function(tree, robots, response, pageUrl, customData) {},
+      junk: function(result, customData) {},
+      link: function(result, customData) {
+        console.log(result);
+        if (!result.broken) {
+          data.sucessUrls.push(result.url.resolved);
+        } else {
+          data.faliureUrls.push(result.url.resolved);
+        }
+      },
+      page: function(error, pageUrl, customData) {},
+      site: function(error, siteUrl, customData) {},
+      end: function() {
+        data.numberOfFailed = data.faliureUrls.length;
+        data.numberOfSucess = data.sucessUrls.length;
+        res.send(data);
+        console.log('done');
+      },
+    },
+  );
+  siteChecker.enqueue('http://' + req.body.url, {
+    'user-agent': 'node-spider',
+  });
 };
-
-
