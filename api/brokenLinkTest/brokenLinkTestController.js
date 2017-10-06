57e4a15b1b7f9c406a4087bd9be95d77a7706915
amansawhney <amansawhney@sawhneygames.com>
2017-10-06 01:55:08 -0400

    added link limiting

--- api/brokenLinkTest/brokenLinkTestController.js
+++ api/brokenLinkTest/brokenLinkTestController.js
@@ -22,6 +22,12 @@ exports.getBrokenLinks = (req, res, next) => {
         } else {
           data.faliureUrls.push(result.url.resolved);
         }
+        if (data.sucessUrls.length + data.faliureUrls.length === 200) {
+          res.send({
+            numberOfFailed: data.numberOfFailed,
+            faliureUrls: data.faliureUrls,
+          });
+        }
       },
       page: function(error, pageUrl, customData) {
         if (error) {
@@ -37,10 +43,12 @@ exports.getBrokenLinks = (req, res, next) => {
       },
       end: function() {
         data.numberOfFailed = data.faliureUrls.length;
-        res.send({
-          numberOfFailed: data.numberOfFailed,
-          faliureUrls: data.faliureUrls,
-        });
+        if (data.sucessUrls.length + data.faliureUrls.length < 200) {
+          res.send({
+            numberOfFailed: data.numberOfFailed,
+            faliureUrls: data.faliureUrls,
+          });
+        }
         console.log('done');
       },
     },
