20dbedcb2ac37b8329e44d4ec94deaa66d729d53
amansawhney <amansawhney@sawhneygames.com>
2017-10-06 19:01:41 -0400

    Updated Links

--- api/brokenLinkTest/brokenLinkTestController.js
+++ api/brokenLinkTest/brokenLinkTestController.js
@@ -22,7 +22,7 @@ exports.getBrokenLinks = (req, res, next) => {
         } else {
           data.faliureUrls.push(result.url.resolved);
         }
-        if (data.sucessUrls.length + data.faliureUrls.length === 100) {
+        if (data.sucessUrls.length + data.faliureUrls.length == 100) {
             res.send({
                 numberOfFailed: data.numberOfFailed,
                 faliureUrls: data.faliureUrls,
